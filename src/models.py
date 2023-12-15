7fba18cb49dee6017624c4bf12467052ac925162
Raul-madero <127880270+Raul-madero@users.noreply.github.com>
2023-12-15 00:07:44 +0000

    Modelado starWars

--- src/models.py
+++ src/models.py
@@ -28,5 +28,55 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Usuario(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False)
+    last_name = Column(String(45), nullable=True)
+    email = Column(String(100), nullable=False)
+    password = Column(String(60), nullable=False)
+
+class Favorites(Base):
+    __tablename__="favorites"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user = relationship(Usuario)
+
+class Characters(Base):
+    __tablename__="characters"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    hair_color = Column(String(25), nullable=True)
+    skin_color = Column(String(25), nullable=True)
+    eye_color = Column(String(25), nullable=True)
+    birth_year = Column(String(25), nullable=False)
+    gender = Column(String(25), nullable=False)
+    created = Column(String(100), nullable=False)
+    edited = Column(String(100), nullable=False)
+    homeworld = Column(String(100), nullable=False)
+    url = Column(String(100), nullable=False)
+
+class Planets(Base):
+    __tablename__="planets"
+    id = Column(Integer, primary_key=True)
+    diameter = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+    gravity = Column(String(45), nullable=False)
+    population = Column(Integer, nullable=True)
+    climate = Column(String(45), nullable=True)
+    terrain = Column(String(45), nullable=True)
+    surface_water = Column(Integer, nullable=True)
+    created = Column(String(60), nullable=False)
+    edited = Column(String(60), nullable=False)
+    name = Column(String(60), nullable=False)
+    url = Column(String(60), nullable=False)
+
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
